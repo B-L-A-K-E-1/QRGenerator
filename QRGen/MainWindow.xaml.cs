e97e3f9da611e6a9887e5ee2e315fe4c159363c1
petr.batha <petr.batha@VT2-12>
2022-03-01 09:02:19 +0100

    Přidejte soubory projektu.

---
+++ QRGen/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using QRCoder;
+using QRCoder.Xaml;
+
+namespace QRGen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string zadanyText = Text_box_vstup.Text;
+            QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
+
+            QRCodeData dataQRkodu = qRCodeGenerator.CreateQrCode(zadanyText, QRCodeGenerator.ECCLevel.M);
+
+            XamlQRCode qrKod = new XamlQRCode(dataQRkodu);
+
+            DrawingImage vkladanyObrazek = qrKod.GetGraphic(30);
+            Obrazek_vystup.Source = vkladanyObrazek;
+            
+        }
+    }
+}
